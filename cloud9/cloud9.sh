073dfae9e9e0e8cebed0348956bb0132d3e8cbca
TakahiroTsukida119 <tsukida@arsaga.jp>
2023-05-08 17:00:18 +0900

    scriptを追加

---
+++ cloud9/cloud9.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# install docker-compose
+sudo mkdir -p /usr/local/lib/docker/cli-plugins
+sudo curl -L https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/lib/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+sudo ln -s /usr/local/lib/docker/cli-plugins/docker-compose /usr/bin/docker-compose
