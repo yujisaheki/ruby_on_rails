81a01db34bf2421e1aa4421cfd63407c6594a177
TakahiroTsukida119 <tsukida@arsaga.jp>
2023-05-12 10:30:54 +0900

    yarnのインストールコマンドを追加

--- cloud9/cloud9.sh
+++ cloud9/cloud9.sh
@@ -5,3 +5,7 @@ sudo mkdir -p /usr/local/lib/docker/cli-plugins
 sudo curl -L https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/lib/docker/cli-plugins/docker-compose
 sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 sudo ln -s /usr/local/lib/docker/cli-plugins/docker-compose /usr/bin/docker-compose
+
+# yarn install
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+sudo yum install yarn
