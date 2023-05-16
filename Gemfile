2885c104ecff6ff55efebd91e6e6e44809437cb2
TakahiroTsukida119 <la.tsukida@gmail.com>
2023-05-17 01:07:58 +0900

    scriptの修正

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ gem "bootsnap", require: false
 
 gem 'mysql2'
 gem 'devise'
+gem 'dotenv-rails'
 # gem 'webpacker'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
--- cloud9/cloud9.sh
+++ cloud9/cloud9.sh
@@ -2,26 +2,53 @@
 
 # package install
 sudo yum update -y
-#sudo yum -y install gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel libffi-devel libxml2 libxslt libxml2-devel libxslt-devel sqlite-devel
+sudo yum -y install gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel libffi-devel libxml2 libxslt libxml2-devel libxslt-devel sqlite-devel
 
-# rubyが直インストールで必要な場合はrbenvを入れるので以下のコメントアウトを外してください
-# ruby setup
-#mkdir -p ~/.rbenv
-#git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-#echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-#echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-#source ~/.bash_profile
-#git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+# install rbenv
+if ! command -v rbenv &> /dev/null; then
+  mkdir -p ~/.rbenv
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+  # shellcheck disable=SC2016
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+  # shellcheck disable=SC2016
+  echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+  source ~/.bash_profile
+fi
 
-# rbenv 3.2.2
-#RUBY_BUILD_BUILD_OPTS=--verbose rbenv install 3.2.2
-#rbenv global 3.2.2
+# install ruby
+RUBY_BUILD_BUILD_OPTS=--verbose rbenv install 3.2.2
+rbenv global 3.2.2
+
+# uninstall rvm
+if command -v rvm &> /dev/null; then
+  rvm seppuku --force
+  source ~/.bash_profile
+fi
+
+# install rails
+printf "install: --no-rdoc --no-ri\nupdate:  --no-rdoc --no-ri\n" >> ~/.gemrc
+gem install rails -v 7.0.4
+
+# uninstall MariaDB, install MySQL 5.7
+sudo service mariadb stop
+sudo yum -y erase mariadb-*
+sudo yum -y localinstall https://repo.mysql.com/mysql80-community-release-el7-5.noarch.rpm
+sudo yum-config-manager --disable mysql80-community
+sudo yum-config-manager --enable mysql57-community
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+sudo yum -y install mysql-community-server mysql-community-devel
+sudo systemctl start mysqld.service
+sudo systemctl enable mysqld.service
+
+DB_PASSWORD=$(sudo grep "A temporary password is generated" /var/log/mysqld.log | sed -s 's/.*root@localhost: //')
+mysql -u root -p${DB_PASSWORD} --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'TB@shibuya1';uninstall plugin validate_password;set password for root@localhost=password('');"
 
 # install docker-compose
-sudo mkdir -p /usr/local/lib/docker/cli-plugins
-sudo curl -L https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/lib/docker/cli-plugins/docker-compose
-sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-sudo ln -s /usr/local/lib/docker/cli-plugins/docker-compose /usr/bin/docker-compose
+#sudo mkdir -p /usr/local/lib/docker/cli-plugins
+#sudo curl -L https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/lib/docker/cli-plugins/docker-compose
+#sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+#sudo ln -s /usr/local/lib/docker/cli-plugins/docker-compose /usr/bin/docker-compose
 
 # yarn install
 #curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
@@ -30,4 +57,4 @@ sudo ln -s /usr/local/lib/docker/cli-plugins/docker-compose /usr/bin/docker-comp
 # node version
 nvm install 16.0.0
 nvm use 16.0.0
-#nvm alias default v16.0.0
+nvm alias default v16.0.0
