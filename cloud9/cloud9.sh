97c830a1d7fba97ddea998dc6597681c04b1dc42
TakahiroTsukida119 <la.tsukida@gmail.com>
2023-05-16 13:36:18 +0900

    fix

--- cloud9/cloud9.sh
+++ cloud9/cloud9.sh
@@ -2,26 +2,26 @@
 
 # package install
 sudo yum update -y
-sudo yum -y install gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel libffi-devel libxml2 libxslt libxml2-devel libxslt-devel sqlite-devel
+#sudo yum -y install gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel libffi-devel libxml2 libxslt libxml2-devel libxslt-devel sqlite-devel
 
 # rubyが直インストールで必要な場合はrbenvを入れるので以下のコメントアウトを外してください
 # ruby setup
-mkdir -p ~/.rbenv
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-source ~/.bash_profile
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+#mkdir -p ~/.rbenv
+#git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+#echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+#echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+#source ~/.bash_profile
+#git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 # rbenv 3.2.2
-RUBY_BUILD_BUILD_OPTS=--verbose rbenv install 3.2.2
-rbenv global 3.2.2
+#RUBY_BUILD_BUILD_OPTS=--verbose rbenv install 3.2.2
+#rbenv global 3.2.2
 
 # install docker-compose
-#sudo mkdir -p /usr/local/lib/docker/cli-plugins
-#sudo curl -L https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/lib/docker/cli-plugins/docker-compose
-#sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-#sudo ln -s /usr/local/lib/docker/cli-plugins/docker-compose /usr/bin/docker-compose
+sudo mkdir -p /usr/local/lib/docker/cli-plugins
+sudo curl -L https://github.com/docker/compose/releases/download/v2.17.3/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/lib/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+sudo ln -s /usr/local/lib/docker/cli-plugins/docker-compose /usr/bin/docker-compose
 
 # yarn install
 #curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
@@ -29,4 +29,5 @@ rbenv global 3.2.2
 
 # node version
 nvm install 16.0.0
+nvm alias default v16.0.0
 nvm use 16.0.0
